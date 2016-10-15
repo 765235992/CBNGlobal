7247f4742605768d20904a34431b071f70dc0da6
Jone <Jone@JonedeMacBook-Pro.local>
2016-10-15 13:21:35 +0800

    Tectonic framework in developer branch

---
+++ CBNGlobal/CBNGlobal/Module/BaseModule/Controller/CBNBaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CBNBaseNavigationController.h
+//  CBNGlobal
+//
+//  Created by Jone on 16/10/15.
+//  Copyright © 2016年 上海第一财经传媒有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBNBaseNavigationController : UINavigationController
+
+@end
